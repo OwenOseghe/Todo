6fe2f2d8e1fb488279ba6ce99f30e5ec6bba9efc
Owen Oseghe <owen.oseghe@htl-wels.at>
2024-06-27 12:05:09 +0200

    feat: add router config with example user router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { log } from 'console';
-import envSchema from './util/env.util';
 import envUtil from './util/env.util';
-console.log(envUtil.PORT);
-console.log(typeof envUtil.PORT)
+import express from 'express';
+import router from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/", router);
+
+app.listen(envUtil.PORT, () => {
+    console.log('Server listening on da port ', envUtil.PORT);
+});
